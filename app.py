ecdcbabd5fe27f27aff3ae5e9fb04c205bb86009
Etien Ruhl <etien_ruhl@live.nl>
2020-07-26 21:05:54 +0200

    First Python Code

---
+++ app.py
@@ -0,0 +1,34 @@
+# Immaginary dog
+
+# print('o----')
+# print(' ||||')
+# print('*' * 10)
+
+# Just a bunch of messy test code
+
+# full_name = 'John Smith'
+# age = 20
+# is_new_patient = True
+
+# name = input('What is your name?! ')
+# print('Hi ' + name)
+#
+# persons_name = input('What\'s your name?! ')
+# favorite_color = input('What\'s your favorite color?!')
+#
+# print(persons_name + ' likes ' + favorite_color + ' a lot')
+
+# year = input('geboortejaar?!: ')
+# age = 2019 - int(year)
+# print(age)
+
+# weight_in_lbs = input('Weight (in lbs): ')
+# weight_in_kg = float(weight_in_lbs) * 0.45
+#
+# print('Weight in kgs is: ' + str(weight_in_kg))
+
+# string = 'deze string is'
+# geclonede_string = string[:]
+
+# string = 'HelloWorld'
+# print(string[1:-1])
